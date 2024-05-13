31f676248c2834b57f839e83b7824f975f88f12a
Naresh2129 <nareshboligorla@gmail.com>
2024-05-13 21:20:13 +0530

    subnets creation

--- main.tf
+++ main.tf
@@ -16,3 +16,31 @@ resource "aws_internet_gateway" "igw" {
     Name = "main"
   }
 }
+
+resource "aws_route" "igw" {
+  for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  route_table_id            = each.value["id"]
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = "aws_internet_gateway.igw.id"
+}
+resource "aws_eip" "ngw" {
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count      = local.public_subnet_ids
+  allocation_id = element(aws_eip.ngw.*.id, count.index)
+  subnet_id     = element(local.public_subnet_ids, count.index)
+}
+
+resource "aws_route" "ngw" {
+  count                    = local.private_route_table_ids
+  route_table_id = element(local.private_route_table_ids, count.index )
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
+}
+
+output "subnet" {
+  value = module.subnets
+}
