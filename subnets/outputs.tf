f2d7716724bf7eb5f4ed71a0f56141a7af0b3e4d
naresh <nareshboligorla@gmail.com>
2024-10-04 11:52:32 +0530

    a

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,11 +1,7 @@
-# output "subnet_ids" {
-#   value = "aws_subnet.main"
-# }
-#
-# output "route_table_ids" {
-#   value = "aws_route_table.main"
-# }
-output "public_subnet_ids" {
-  value = "local.public_subnet_ids"
+output "subnet_ids" {
+  value = aws_subnet.main
 }
 
+output "route_table_ids" {
+  value = aws_route_table.main
+}
