07100d7dcc9801b69871bd36c5bb53ba35d54216
naresh <nareshboligorla@gmail.com>
2024-12-13 09:43:52 +0530

    creation of subnets

--- subnets/main.tf
+++ subnets/main.tf
@@ -3,7 +3,16 @@ resource "aws_subnet" "main" {
   vpc_id     = var.vpc_id
   cidr_block = each.value["cidr"]
   availability_zone = each.value["az"]
+}
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id = var.vpc_id
+  tags = { Name = each.key }
+}
 
 
-
+resource "aws_route_table_association" "a" {
+  for_each = var.subnets
+  subnet_id      = lookup(lookup(aws_subnet.main, each.key, null), "id", null)
+  route_table_id = lookup(lookup(aws_route_table.main, each.key, null), "id", null)
 }
