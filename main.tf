5ca4d3e56a7322f35f3bc597394e1184f46ab4e3
naresh <nareshboligorla@gmail.com>
2024-12-16 12:14:12 +0530

    route table entries in natgateway

--- main.tf
+++ main.tf
@@ -29,3 +29,9 @@ resource "aws_nat_gateway" "ngw" {
   allocation_id = element(aws_eip.ngw.*.id, count.index)
   subnet_id     = element(local.public_subnet_ids, count.index)
 }
+resource "aws_route" "ngw" {
+  count                  = length(local.private_route_table_ids)
+  route_table_id         = element(local.private_route_table_ids, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
+}
